df1de80c04499552df2ca59768edff47ea01bcbb
mario <mario@mario-PC.(none)>
2016-11-16 15:05:39 +0100

    tabella si aggiorna/spazio disponibile corretto

--- magazzino.js
+++ magazzino.js
@@ -1,60 +1,54 @@
 /*
 *Script che gestisce un magazzino temporaneo su una pagina
 */
-var magazzino = { pesche:3 , mele:4 , pere:5 } 
-var max = 20;
-
+var magazzino = [ "pesche", "mele", "pere" ]; //contenitore oggetti
+var Q = [3,3,3];                              //quantità degli oggetti
+var max = 20.0;
+var qnt = 9.0;
+/**
+ * @brief mostra lo spazio disponibile
+ * @return null
+ */
 function space_left(){
-    var c = 0;
-    for (key in magazzino){
-            c += parseInt(magazzino[key]);
-        };
-    c = max - c;
-    
     //aggiorno il contatore sulla pagina
-    var cont = document.getElementById("available");
-    cont.innerHTML = "Available Space - " + c.toString();
-    
-    if (c > max){
-        allert("Il magazzino é troppo pieno!");
-    }
-}
+    var cont = document.getElementById("available");        //prendo l'oggetto
+    cont.innerHTML = "Available Space: " + (max-qnt).toString();  //aggiorno l html
+};
 
 /**
- * @brief inizializza un magazzino di default e li visualizza sulla pagina
+ * @brief inizializza visualmente il magazzino di default e li visualizza sulla pagina
  * @return null
  */
 function init(){
-    //provo a vedere se la tabella c'é già
+    //prendo la tabella 
     var table = document.getElementById("myTable");
-    //se non c'é la creo
-    if (table == null){
-        table = document.createElement("TABLE");
-        table.setAttribute("id", "myTable");    //imposto l'id
-        document.body.appendChild(table);       //appendo la tabella al bpdy
-        
-        var row = table.insertRow(0);           //nuova riga
+    //prima cancello se c'erà un eventuale riga
+    document.getElementById("myTable").deleteRow(-1);
+    var row = table.insertRow(0);           //nuova riga
     
-        for (key in magazzino){
-            var cell = row.insertCell(-1);                  //appendo una cella in fondo alla riga
-            cell.innerHTML = key + " - " + magazzino[key];  //inserisco il prodotto
-            cell.style.border = "1px solid #000"
-        };
-    };
+    for (i=0; i<magazzino.length; i++){
+        var cell = row.insertCell(-1);                  //appendo una cella in fondo alla riga
+        cell.innerHTML = magazzino[i] + " - " + Q[i];  //inserisco il prodotto
+        cell.style.border = "1px solid #000";
+    }
+        
     space_left();
 };
 
 /**
  * @brief aggiunge i campi per poter ordinare altri prodotti
- * @return Description of returned value.
+ * @return null
  */
 function newOrder(){
+    //prendo gli oggetti della pagina
     var button = document.getElementById("btnOrder");
     var lblProduct = document.getElementById("lblProduct");
     var txtbProduct = document.getElementById("txtbProduct");
     var lblAmount = document.getElementById("lblAmount");
     var txtbAmount = document.getElementById("txtbAmount");
     
+    //solo se non sono già stati aggiunti
+    //gli aggiungo uno ad uno
     if (lblProduct == null){
         var lbl = document.createElement("B");
         lbl.setAttribute("id", "lblProduct");
@@ -90,30 +84,48 @@ function newOrder(){
         button = document.createElement("button");
         button.setAttribute("id", "btnOrder");
         button.setAttribute("onClick", "order()");
-        var text = document.createTextNode("Order more!");
+        var text = document.createTextNode("Ordina Prodotto!");
         button.appendChild(text);
         document.body.appendChild(button);
     }
 };
 
+/**
+ * @brief ordina un'altro oggetto e informa se non c'é più spazio
+ * @return null
+ */
 function order(){
+    //carico i dati del prodotto da ordinare
     var key = document.getElementById("txtbProduct").value;
-    var amount = document.getElementById("txtbAmount").value;
-    var chk = false;
+    var amount = parseFloat(document.getElementById("txtbAmount").value);
+    var check = false;
     
-    for(k in magazzino){
-        if (k == key){
-            magazzino[k] += amount;
-            chk = true;
-        };
-    };
-    
-    if (chk == false){
-        magazzino[key] = amount;
+    for(i=0; i<magazzino.length; i++){
+        //controllo per tutti i prodotti 
+        //se ci sono già
+        if (magazzino[i] == key){
+            //aggiorno il magazzino
+            Q[i] += amount;
+            qnt += amount; //aggiorno spazio restante
+            check = true;   
+        }
+    }
+     
+    //se il prodotto non c'era già
+    if (check == false){
+        window.alert(key + " " + amount);
+        //aggiungo il nuovo prodotto in magazzino
+        magazzino[magazzino.length]=key;
+        Q[Q.length]=amount;
+        //aggiorno cmq lo spazio
+        qnt += amount;
+    }
+    //informo l'utente che lo spazio non é sufficiente
+    if (qnt >= max){
+        window.alert("Attention! There is no more space!");    
     }
     space_left();
+    init();
 };
-
-
 
 
